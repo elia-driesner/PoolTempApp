627fb9152c488fd326e540d2515d727f1894a932
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2022-06-24 16:59:53 +0200

    finished

--- pool_temp/lib/main.dart
+++ pool_temp/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
+import 'dart:async';
 
 import 'thermometer_widget.dart';
 
@@ -15,9 +18,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Pool Temperature',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: const Color(0xFFFFFFFF)),
       home: HomePage(),
     );
   }
@@ -29,27 +32,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String temp = '';
-  String serverStatus = '';
+  String temp = '0.0';
+  String serverStatus = 'Verbinde';
+  var waveColor = Color.fromARGB(255, 124, 124, 124).withOpacity(0.3);
 
   void getPoolTemp() async {
-    setState(() {
-      serverStatus = 'Verbinde...';
-    });
     try {
-      final response =
-          await http.get(Uri.parse('http://127.0.0.1:5000/poolTemp'));
+      final response = await http
+          .get(Uri.parse('https://drmainserver.pagekite.me//poolTemp'));
       final decoded = json.decode(response.body) as Map<String, dynamic>;
 
-      setState(() {
-        temp = decoded['tC'];
-        serverStatus = 'Verbunden';
-      });
+      if (decoded['tC'] != '0.0') {
+        setState(() {
+          temp = decoded['tC'];
+          serverStatus = 'Verbunden';
+          waveColor = Colors.blueAccent.withOpacity(0.3);
+        });
+      } else {
+        setState(() {
+          serverStatus = 'Sensor Offline';
+          waveColor = Color.fromARGB(255, 124, 124, 124).withOpacity(0.3);
+        });
+      }
     } catch (e) {
-      setState(() {
-        temp = '0.0';
-        serverStatus = 'Server Offline';
-      });
+      if (temp != '0.0') {
+        setState(() {
+          serverStatus = 'Server Offline';
+          waveColor = Color.fromARGB(255, 124, 124, 124).withOpacity(0.3);
+        });
+      } else {
+        setState(() {
+          temp = '0.0';
+          serverStatus = 'Server Offline';
+          waveColor = Color.fromARGB(255, 124, 124, 124).withOpacity(0.3);
+        });
+      }
     }
   }
 
@@ -59,12 +76,44 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     WidgetsBinding.instance?.addPostFrameCallback((_) {
       getPoolTemp();
+      Timer mytimer = Timer.periodic(Duration(seconds: 10), (timer) {
+        getPoolTemp();
+      });
     });
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+        body: Stack(children: [
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 300,
+          child: WaveWidget(
+            config: CustomConfig(
+              colors: [
+                waveColor, waveColor, waveColor,
+                //the more colors here, the more wave will be
+              ],
+              durations: [10000, 9000, 80000],
+              //durations of animations for each colors,
+              // make numbers equal to numbers of colors
+              heightPercentages: [0.01, 0.01, 0.03],
+              //height percentage for each colors.
+              blur: MaskFilter.blur(BlurStyle.solid, 5),
+              //blur intensity for waves
+            ),
+            waveAmplitude: 5.00, //depth of curves
+            waveFrequency: 3, //number of curves in waves
+            backgroundColor: Colors.white, //background colors
+            size: Size(
+              double.infinity,
+              double.infinity,
+            ),
+          ),
+        ),
+      ),
+      Container(
         child:
             Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
           Center(
@@ -81,7 +130,7 @@ class _HomePageState extends State<HomePage> {
                       Text(
                         '$temp',
                         style: TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold),
+                            fontSize: 43, fontWeight: FontWeight.bold),
                       ),
                       Text(
                         '°C',
@@ -98,16 +147,21 @@ class _HomePageState extends State<HomePage> {
               child: Column(
             children: [
               Text('Server Status:',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
               serverStatus == 'Verbunden'
                   ? Text(serverStatus,
-                      style: TextStyle(fontSize: 20, color: Colors.green))
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 105, 244, 110)))
                   : Text(serverStatus,
                       style: TextStyle(fontSize: 20, color: Colors.red))
             ],
           ))
         ]),
       ),
-    );
+    ]));
   }
 }
